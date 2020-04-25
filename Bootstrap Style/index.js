21022b84d17defead5d4011822b89a3249ff50f1
Vlad Badea <vladbadea491@gmail.com>
2020-04-25 12:31:30 +0200

    working on responsive background image

---
+++ Bootstrap Style/index.js
@@ -0,0 +1,6 @@
+function backgroundImage(){
+    var x = document.getElementById("backgroundImage");
+    var z = document.getElementById("navbarButton");
+    var size = document.documentElement.clientWidth;
+    console.log(size);
+};
