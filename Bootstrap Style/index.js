35577549400997fadf1498f8973652284ff6f9c6
Vlad Badea <vladbadea491@gmail.com>
2020-04-26 18:42:44 +0200

    working on navbar

--- Bootstrap Style/index.js
+++ Bootstrap Style/index.js
@@ -1,11 +1,21 @@
 function backgroundImage(){
     var x = document.getElementById("backgroundImage");
+    var gallery = document.getElementById("navbarGallery");
+    var projects = document.getElementById("navbarProjects");
     var screenWidth = document.documentElement.clientWidth;
     var screenHeight = document.documentElement.clientHeight;
+    console.log("ciao");
     if (screenWidth > screenHeight){
         x.className = "img-fluid vh-100";
     } else {
         x.className = "img-fluid vw-100";
     }
-
+    if (screenWidth >= 768){
+        gallery.style.marginLeft = "-250%";
+        projects.style.marginLeft = "250%"; 
+    } else {
+        gallery.style.marginLeft = "0%";
+        projects.style.marginLeft = "0%";
+    }
+    console.log(screenWidth);
 };
