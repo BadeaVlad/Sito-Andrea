99aedbd6902dd364b44c6c09b9348edaa32bb471
Vlad Badea <vladbadea491@gmail.com>
2020-04-28 11:21:49 +0200

    semi-fixed margin-left of the svg

--- Bootstrap Style/index.js
+++ Bootstrap Style/index.js
@@ -2,8 +2,11 @@ function backgroundImage(){
     var x = document.getElementById("backgroundImage");
     var gallery = document.getElementById("navbarGallery");
     var projects = document.getElementById("navbarProjects");
+    var firmaCalligrafica = document.getElementById("firmaCalligrafica");
     var screenWidth = document.documentElement.clientWidth;
     var screenHeight = document.documentElement.clientHeight;
+    var svgMarginLeft = (screenWidth-200)/2;
+    firmaCalligrafica.style.marginLeft = svgMarginLeft + "px";
     console.log("ciao");
     if (screenWidth > screenHeight){
         x.className = "img-fluid vh-100";
