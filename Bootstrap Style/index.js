1096c960d79ec5d1d985cc4d41df5408306abb1d
Vlad Badea <vladbadea491@gmail.com>
2020-05-02 21:09:23 +0200

    q

--- Bootstrap Style/index.js
+++ Bootstrap Style/index.js
@@ -5,20 +5,20 @@ function backgroundImage(){
     var firmaCalligrafica = document.getElementById("firmaCalligrafica");
     var screenWidth = document.documentElement.clientWidth;
     var screenHeight = document.documentElement.clientHeight;
-    var svgMarginLeft = (screenWidth-200)/2;
-    firmaCalligrafica.style.marginLeft = svgMarginLeft + "px";
+    //var svgMarginLeft = (screenWidth-200)/2;
+    firmaCalligrafica.style.marginLeft = "50px";
     console.log("ciao");
     if (screenWidth > screenHeight){
         x.className = "img-fluid vh-100";
     } else {
         x.className = "img-fluid vw-100";
     }
-    if (screenWidth >= 768){
+    /*if (screenWidth >= 768){
         gallery.style.marginLeft = "-250%";
         projects.style.marginLeft = "250%"; 
     } else {
         gallery.style.marginLeft = "0%";
         projects.style.marginLeft = "0%";
-    }
+    }*/
     console.log(screenWidth);
 };
