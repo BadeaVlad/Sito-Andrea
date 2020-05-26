e7d0ca6a72ca212b178a14b5e584c416b6961230
Vlad Badea <vladbadea491@gmail.com>
2020-05-26 13:26:51 +0200

    o

--- Bootstrap Style/index.js
+++ Bootstrap Style/index.js
@@ -1,4 +1,5 @@
 function init(){
+    timer();
     backgroundImage();
     navbar();
 };
@@ -16,14 +17,35 @@ function backgroundImage(){
         x.style.width = screenWidth;
     }
     console.log(screenWidth);
+
 };
 
 function navbar(){
     var gallery = document.getElementById("navbarGallery");
     var projects = document.getElementById("navbarProjects");
+    var wallpaper = document.getElementById("navbarWallpaper");
+    var navbar = document.getElementById("navbar");
     var firmaCalligrafica = document.getElementById("firmaCalligrafica");
-    gallery.style.color = "white";
-    projects.style.color = "white";
+    navbar.style.opacity = "0.7";
+    gallery.style.color = "black";
+    wallpaper.style.color = "black";
+    projects.style.color = "black";
     gallery.style.textDecoration = "none";
+    wallpaper.style.textDecoration = "none";
     projects.style.textDecoration = "none";
 };
+
+function randomizerImg(){
+    var x = document.getElementById("backgroundImage");
+    var backgroungImages = ["_HLX2852.jpg", "_HLX4827.jpg", "_HLX5342.jpg", "3.jpg", "4.jpg", "10.jpg", "16.jpg", "Tramonto 3.jpg", "Tunnel 2.jpg"];
+    var backgroundColors = ["#A6754B", "#A68F86", "#595959", "#735A55", "#261813", "#8C6456", "#45788C", "#D9946C", "#59372A"];
+    var bodyBackgroundColor = document.getElementById("body");
+    var i = Math.floor( Math.random()*8);
+    x.src = backgroungImages[i];
+    bodyBackgroundColor.style.backgroundColor = backgroundColors[i];
+    x.animate
+};
+function timer(){
+    window.setInterval("randomizerImg()", 5000);
+};
+
